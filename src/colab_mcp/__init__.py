19cee016a54e50a59f032029cac3253259d0c447
Tyler <rtp@google.com>
2026-03-24 16:28:32 -0700

    Cleanup a few things not needed in this project (coming later elsewhere).

--- src/colab_mcp/__init__.py
+++ src/colab_mcp/__init__.py
@@ -22,8 +22,6 @@ import sys
 from fastmcp import FastMCP
 from fastmcp.utilities import logging as fastmcp_logger
 
-from colab_mcp import runtime
-from colab_mcp import auth
 from colab_mcp.session import ColabSessionProxy
 
 
@@ -56,25 +54,11 @@ def parse_args(v):
         default=tempfile.mkdtemp(prefix="colab-mcp-logs-"),
     )
     parser.add_argument(
-        "-r",
-        "--enable-runtime",
-        help="if set, export tools to talk directly to the Colab Jupyter runtime (disabled by default).",
-        action="store_true",
-        default=False,
-    )
-    parser.add_argument(
         "-p",
         "--enable-proxy",
         help="if set, enable the runtime proxy (enabled by default).",
         action="store_true",
         default=True,
-    )
-    parser.add_argument(
-        "-c",
-        "--client-oauth-config",
-        help="client oauth config json",
-        action="store",
-        default="colab-mcp-oauth-config.json",
     )
     return parser.parse_args(v)
 
@@ -82,17 +66,6 @@ def parse_args(v):
 async def main_async():
     args = parse_args(sys.argv[1:])
     init_logger(args.log)
-
-    if args.enable_runtime:
-        # preemptively initialize credentials when we start so they're available
-        try:
-            auth.get_credentials(args.client_oauth_config)
-        except PermissionError as e:
-            sys.exit(f"failed to initialize authentication credentials, exiting - {e}")
-
-        crt = runtime.ColabRuntimeTool()
-        logging.info("enabling runtime tools")
-        mcp.mount(crt.mcp, prefix="runtime")
 
     if args.enable_proxy:
         logging.info("enabling session proxy tools")
@@ -108,10 +81,6 @@ async def main_async():
     finally:
         if args.enable_proxy:
             await session_mcp.cleanup()
-
-        if args.enable_runtime:
-            if crt:
-                crt.stop()
 
 
 def main() -> None:
